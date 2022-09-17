6e920c22d056cac08ae3e62ec6d588e3c061cebe
nat <nathalie.piat1@gmail.com>
2022-09-17 14:32:15 +0200

    Initial Commit

---
+++ PeopleSwiftUI/PeopleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleSwiftUIApp.swift
+//  PeopleSwiftUI
+//
+//  Created by Nova on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeopleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
